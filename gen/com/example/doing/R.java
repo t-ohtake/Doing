53eee29d2863a8f0e47bb981b09954347d5d877d
t-ohtake <t.ohtake@s2soft.co.jp>
2014-01-27 19:09:29 +0900

    レイアウト変更
    ・ボタンを下にした
    ・背景を変な感じに変更

--- gen/com/example/doing/R.java
+++ gen/com/example/doing/R.java
@@ -40,6 +40,7 @@ public final class R {
         public static final int run_onclick=0x7f020010;
         public static final int st=0x7f020011;
         public static final int st_onclick=0x7f020012;
+        public static final int tableback=0x7f020013;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;

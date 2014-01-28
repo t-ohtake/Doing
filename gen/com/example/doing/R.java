076aed21deb15de24cd6f13bb2bfae8279abbd18
t-ohtake <t.ohtake@s2soft.co.jp>
2014-01-28 13:09:26 +0900

    処理変更
    イメージボタンのイベント取得処理追加

--- gen/com/example/doing/R.java
+++ gen/com/example/doing/R.java
@@ -51,8 +51,12 @@ public final class R {
         public static final int tablebackbuttom=0x7f02001b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int grBtn=0x7f080003;
+        public static final int hmBtn=0x7f080001;
         public static final int linearLayout1=0x7f080000;
+        public static final int runBtn=0x7f080002;
+        public static final int stBtn=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/doing/MainActivity.java
+++ src/com/example/doing/MainActivity.java
@@ -2,23 +2,61 @@ package com.example.doing;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//タイトルバー非表示
 		setContentView(R.layout.activity_main);
+		
+		//ボタン情報を取得
+		ImageButton hmBtn = (ImageButton)findViewById(R.id.hmBtn);
+		ImageButton runBtn = (ImageButton)findViewById(R.id.runBtn);
+		ImageButton grBtn = (ImageButton)findViewById(R.id.grBtn);
+		ImageButton stBtn = (ImageButton)findViewById(R.id.stBtn);
+		
+		//各ボタンが押された時
+		hmBtn.setOnClickListener(this);
+		runBtn.setOnClickListener(this);
+		grBtn.setOnClickListener(this);
+		stBtn.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void onClick(View v)
+	{
+		switch(v.getId())
+		{
+			case R.id.hmBtn:
+				System.out.println("hmBtn");
+			    break;
+			case R.id.runBtn:
+				System.out.println("runBtn");
+			    break;
+			case R.id.grBtn:
+				System.out.println("grBtn");
+			    break;
+			case R.id.stBtn:
+				System.out.println("stBtn");
+			    break;
+		}		
+    }
 
 }

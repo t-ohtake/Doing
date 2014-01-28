db2dd2ba541c2dff0897c2840123657f32d4d893
t-ohtake <t.ohtake@s2soft.co.jp>
2014-01-28 22:09:59 +0900

    2014/01/28　終了コミット
    アイコンは消えるけど、矢印アイコンが変なとこにいく

--- gen/com/example/doing/R.java
+++ gen/com/example/doing/R.java
@@ -49,14 +49,17 @@ public final class R {
         public static final int st_onclick2=0x7f020019;
         public static final int tableback=0x7f02001a;
         public static final int tablebackbuttom=0x7f02001b;
+        public static final int ue=0x7f02001c;
+        public static final int uedeka=0x7f02001d;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int grBtn=0x7f080003;
         public static final int hmBtn=0x7f080001;
         public static final int linearLayout1=0x7f080000;
         public static final int runBtn=0x7f080002;
         public static final int stBtn=0x7f080004;
+        public static final int ueBtn=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/doing/MainActivity.java
+++ src/com/example/doing/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity implements OnClickListener
 {
@@ -21,12 +22,14 @@ public class MainActivity extends Activity implements OnClickListener
 		setContentView(R.layout.activity_main);
 		
 		//ボタン情報を取得
+		ImageButton ueBtn = (ImageButton)findViewById(R.id.ueBtn);
 		ImageButton hmBtn = (ImageButton)findViewById(R.id.hmBtn);
 		ImageButton runBtn = (ImageButton)findViewById(R.id.runBtn);
 		ImageButton grBtn = (ImageButton)findViewById(R.id.grBtn);
 		ImageButton stBtn = (ImageButton)findViewById(R.id.stBtn);
 		
 		//各ボタンが押された時
+		ueBtn.setOnClickListener(this);
 		hmBtn.setOnClickListener(this);
 		runBtn.setOnClickListener(this);
 		grBtn.setOnClickListener(this);
@@ -44,6 +47,11 @@ public class MainActivity extends Activity implements OnClickListener
 	{
 		switch(v.getId())
 		{
+			//矢印ボタンを押下
+			case R.id.ueBtn:
+				//フッター非表示、表示処理
+				iconEraser();
+		    break;
 			case R.id.hmBtn:
 				System.out.println("hmBtn");
 			    break;
@@ -58,5 +66,27 @@ public class MainActivity extends Activity implements OnClickListener
 			    break;
 		}		
     }
+	
+	/**
+	 *　フッターのアイコンを表示したり、消したり 
+	 */
+	public void iconEraser()
+	{
+		//リニアレイアウト（フッターのレイアウト郡)
+		LinearLayout linearLayout = (LinearLayout)findViewById(R.id.linearLayout1);
+		
+		//ホームアイコンが表示されていたら非表示にする、非表示ならすべてのアイコンを表示する
+		if (linearLayout.getVisibility() == 0)
+		{
+			linearLayout.setVisibility(View.INVISIBLE);
+			linearLayout.setVisibility(View.GONE);
+			System.out.println("フッターアイコンを非表示");
+		}
+		else
+		{
+			linearLayout.setVisibility(View.VISIBLE);
+			System.out.println("フッターアイコンを表示");
+		}
+	}
 
 }
